b65a028e314a8721b3762f8cc0d0f8df0627c570
Lika Datunishvili <likadatunishvili@MacBook-Pro-admin.local>
2026-05-23 19:07:49 +0400

    new project, hierarchy check

--- src/main/java/org/example/Meeting.java
+++ src/main/java/org/example/Meeting.java
@@ -3,47 +3,47 @@ package org.example;
 import java.time.LocalDateTime;
 
 public class Meeting {
-    private Long id;
-    private String title;
-    private LocalDateTime meetingStartTime;
-    private Employee employee;
+   private Long id;
+   private String title;
+   private LocalDateTime meetingStartTime;
+   private Employee employee;
 
-    public Meeting(Long id, String title, LocalDateTime meetingStartTime, Employee employee) {
-        this.id = id;
-        this.title = title;
-        this.meetingStartTime = meetingStartTime;
-        this.employee = employee;
-    }
+   public Meeting(Long id, String title, LocalDateTime meetingStartTime, Employee employee) {
+      this.id = id;
+      this.title = title;
+      this.meetingStartTime = meetingStartTime;
+      this.employee = employee;
+   }
 
-    public Long getId() {
-        return id;
-    }
+   public Long getId() {
+      return id;
+   }
 
-    public void setId(Long id) {
-        this.id = id;
-    }
+   public void setId(Long id) {
+      this.id = id;
+   }
 
-    public String getTitle() {
-        return title;
-    }
+   public String getTitle() {
+      return title;
+   }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
+   public void setTitle(String title) {
+      this.title = title;
+   }
 
-    public LocalDateTime getMeetingStartTime() {
-        return meetingStartTime;
-    }
+   public LocalDateTime getMeetingStartTime() {
+      return meetingStartTime;
+   }
 
-    public void setMeetingStartTime(LocalDateTime meetingStartTime) {
-        this.meetingStartTime = meetingStartTime;
-    }
+   public void setMeetingStartTime(LocalDateTime meetingStartTime) {
+      this.meetingStartTime = meetingStartTime;
+   }
 
-    public Employee getEmployee() {
-        return employee;
-    }
+   public Employee getEmployee() {
+      return employee;
+   }
 
-    public void setEmployee(Employee employee) {
-        this.employee = employee;
-    }
+   public void setEmployee(Employee employee) {
+      this.employee = employee;
+   }
 }
--- src/main/java/org/example/technology.java
+++ src/main/java/org/example/technology.java
@@ -1,5 +1,7 @@
 package org.example;
 
+import java.util.List;
+
 public class technology {
     private Long id;
     private String name;
